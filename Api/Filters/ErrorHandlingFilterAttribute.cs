f13fd498f6fb2dcc0131b1dfa6926f5811564e7e
Grabtot <dneshotkin@gmail.com>
2023-09-05 15:02:58 +0300

    Add error handling filter

---
+++ Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BuberDinner.Api.Filters
+{
+    public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            ProblemDetails problemDetails = new()
+            {
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                Title = "An error occurred whileb  vb  processing your request",
+                Status = StatusCodes.Status500InternalServerError
+            };
+
+            context.Result = new ObjectResult(problemDetails);
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
